34a7a6c4a2ac90c3db80098b67c4f504ae5dc250
Pierre MARTINEAU <pmartine@e2r4p20.42.fr>
2016-01-28 19:30:56 +0100

    maj .h

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: pmartine <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/08 17:12:09 by pmartine          #+#    #+#             */
-/*   Updated: 2016/01/28 19:18:45 by pmartine         ###   ########.fr       */
+/*   Updated: 2016/01/28 19:30:20 by pmartine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,6 @@
 
 # include "libft/includes/libft.h"
 # define BUFF_SIZE 32
-
-typedef struct		s_fd
-{
-	int				fd;
-	char			*text;
-	struct s_fd		*next;
-	struct s_fd		*begin_list;
-}					t_fd;
 
 int					get_next_line(int const fd, char **line);
 #endif
